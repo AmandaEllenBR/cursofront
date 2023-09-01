9de8d51aa130b142e422d95993203ecce4e2830c
amandaellenbr <ammada.reis1404@gmail.com>
2023-09-01 12:02:02 -0300

    Atualizando repositorio

---
+++ portugol/html/ATIVIDADE 10/java.js
@@ -0,0 +1,13 @@
+const titulo = window.document.getElementsByTagName('h1')
+
+console.log(titulo)
+
+//const divis = document.getElementsByClassName('quadrado') 
+//console.log(divis)
+
+//const redonda = document.getElementsByClassName('circulo')
+//console.log(redonda)
+
+
+//const lista = document.querySelectorAll('quadrado')
+//console.log(lista)
---
+++ portugol/html/ATIVIDADE 10/script.js
@@ -0,0 +1,30 @@
+//var numero1
+
+//let numero2
+
+//const numero3 = 10
+
+//var n1 = 'luciano'
+//var n2 = '10'
+//var soma = n1 - n2
+// console.log(typeof(soma))
+// console.log(soma)
+
+
+//var caneca = {
+    nome: 'teu cu',
+    cor: 'colorida',
+    educativa: true
+//}
+//console.log(typeof(caneca))
+//console.log(caneca)
+
+//var exVertor =['caneca' , 'cafe']
+
+//console.log(exVertor)
+//console.log(typeof(exVertor))
+
+
+//var  nome 
+//nome = 10
+//console.log(typeof(nome))
